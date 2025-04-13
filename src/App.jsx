c6d6a9f98dc8d67971567373321aca05ade5bd30
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-04-13 00:47:43 -0300

    Implementações das ações dos button no componente "tasks".

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import AddTask from "./components/AddTask";
+// import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
@@ -24,6 +24,22 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+  function onDeleteTaskClick(taskId) {
+    const deleteTask = tasks.filter((task) => task.id != taskId);
+    setTasks(deleteTask);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -31,7 +47,11 @@ function App() {
           Gerenciador de Tarefas
         </h1>
         {/* <AddTask /> */}
-        <Tasks tasks={tasks} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,15 +1,28 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
-export default function Tasks(props) {
+export default function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 text-left w-full text-white p-2 rounded-md">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`${
+              !task.isCompleted
+                ? "bg-slate-400 text-left w-full text-white p-2 rounded-md"
+                : "bg-slate-400 text-left w-full text-white p-2 rounded-md line-through"
+            }`}
+          >
             {task.title}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
             <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => onDeleteTaskClick(task.id)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
+            <TrashIcon />
           </button>
         </li>
       ))}
