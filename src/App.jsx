a883ffa1d65c68d2d7a68a66fbdc022690fd3759
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-04-13 01:14:08 -0300

    Implementação do componente de adicionar tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import AddTask from "./components/AddTask";
+import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
@@ -35,6 +35,7 @@ function App() {
 
     setTasks(newTasks);
   }
+
   function onDeleteTaskClick(taskId) {
     const deleteTask = tasks.filter((task) => task.id != taskId);
     setTasks(deleteTask);
@@ -42,11 +43,11 @@ function App() {
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-5">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        {/* <AddTask /> */}
+        <AddTask />
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
